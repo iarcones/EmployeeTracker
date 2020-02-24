1e748cf63307aecb3aab360cbce54b5226bbbe40
Isabel <iarcones@gmail.com>
2020-02-24 11:18:26 -0800

    added update

--- app.js
+++ app.js
@@ -10,41 +10,42 @@ function menu () {
                 type: "list",
                 name: "cmd",
                 message: "what do you want?",
-                choices: [ "add", "view", "update", "delete"]
+                choices: [ "add", "view", "update", "delete" ]
             },
             {
                 type: "list",
                 name: "option",
                 message: "VIEW: what do you want?",
-                choices: [ "employee", "role", "department", "employee by manager"],
-                when: function(answer){
+                choices: [ "employee", "role", "department", "employee by manager" ],
+                when: function (answer) {
                     return answer.cmd === "view"
-                 }
+                }
             },
             {
                 type: "list",
                 name: "option",
                 message: "UPDATE: what do you want?",
-                choices: [ "employee role", "employee manager"],
-                when: function(answer){
+                choices: [ "employee role", "employee manager" ],
+                when: function (answer) {
                     return answer.cmd === "update"
-                 }
+                }
             },
             {
                 type: "list",
                 name: "option",
                 // message: "ADD and DELETE: what do you want?",
-                message: function(answer){
-                    if(answer.cmd==="add"){ return "ADD: what do you want?"} else {return " DELETE: what do you want?"}},
-                choices: [ "employee", "role", "department"],
-                when: function(answer){
-                    return answer.cmd === "delete" || answer.cmd === "add" 
-                 }
+                message: function (answer) {
+                    if (answer.cmd === "add") { return "ADD: what do you want?" } else { return " DELETE: what do you want?" }
+                },
+                choices: [ "employee", "role", "department" ],
+                when: function (answer) {
+                    return answer.cmd === "delete" || answer.cmd === "add"
+                }
             }
 
         ])
         .then(({ cmd, option }) => {
-            console.log(cmd, option)
+            console.log("responses:", cmd, option)
 
             switch (cmd) {
                 case "add":
@@ -71,18 +72,18 @@ function menu () {
                         case "department":
                             viewDepartment()
                             break;
+                        case "employee by manager":
+                            viewEmployeeByManager()
+                            break;
                     }
                     break;
                 case "update":
                     switch (option) {
-                        case "employee":
-                            updateEmployee()
+                        case "employee role":
+                            updateEmployeeRole()
                             break;
-                        case "role":
-                            updateRole()
-                            break;
-                        case "department":
-                            updateDepartment()
+                        case "employee manager":
+                            updateEmployeeManager()
                             break;
                     }
                     break;
@@ -194,25 +195,51 @@ async function addEmployee () {
 
 }
 
-async function viewDepartment(){
+async function viewDepartment () {
     const result = await getDepartment()
     console.table(result)
     menu()
 }
 
-async function viewRole(){
+async function viewRole () {
     const result = await getRolesExtended()
     console.table(result)
     menu()
 }
 
-async function viewEmployee(){
+async function viewEmployee () {
     const result = await getEmployeesExtended()
     console.table(result)
     menu()
 }
 
-async function updateEmployee () {
+async function viewEmployeeByManager () {
+    const managerDB = await getManagers()
+    const managerChoices = managerDB.map(elem => {
+        return {
+            name: elem.first_name + " " + elem.last_name,
+            value: elem.id
+        }
+    })
+    const managerRec = await inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: "manager_id",
+                message: "Manager:",
+                choices: managerChoices
+            }
+
+        ])
+
+    result = await getEmployeesbyManagers(managerRec.manager_id)
+  
+    console.table(result)
+    menu()
+}
+
+async function updateEmployeeRole () {
+
     const rolesDB = await getRoles()
     const rolesChoices = rolesDB.map(elem => {
         return {
@@ -227,26 +254,16 @@ async function updateEmployee () {
             value: elem.id
         }
     })
- 
+
 
 
     const employeeRec = await inquirer
         .prompt([
             {
                 type: 'list',
-                name: "manager_id",
-                message: "Manager:",
-                choices: managerChoices
-            },
-            {
-                type: 'input',
-                name: "first_name",
-                message: "First name:",
-            },
-            {
-                type: 'input',
-                name: "last_name",
-                message: "Last name:",
+                name: "employee_id",
+                message: "Employee:",
+                choices: employeeChoices
             },
             {
                 type: 'list',
@@ -254,14 +271,47 @@ async function updateEmployee () {
                 message: "Role:",
                 choices: rolesChoices
             },
-           
+
         ])
 
-    result = await insertEmployee(employeeRec)
+    result = await updateEmployeeRoleDB(employeeRec.employee_id,employeeRec.role_id)
     menu()
 
 }
 
+async function updateEmployeeManager () {
+
+    const employeeDB = await getEmployees()
+    const employeeChoices = employeeDB.map(elem => {
+        return {
+            name: elem.first_name + " " + elem.last_name,
+            value: elem.id
+        }
+    })
+
+
+
+    const employeeRec = await inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: "employee_id",
+                message: "Employee:",
+                choices: employeeChoices
+            },
+            {
+                type: 'list',
+                name: "manager_id",
+                message: "Manager:",
+                choices: employeeChoices
+            },
+
+        ])
+
+    result = await updateEmployeeManagerDB(employeeRec.employee_id,employeeRec.manager_id)
+    menu()
+
+}
 // DB  - connection
 
 const util = require("util");
@@ -296,7 +346,15 @@ function getEmployees () {
     return connection.query("SELECT * FROM employee")
 }
 function getEmployeesExtended () {
-    return connection.query( "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id")
+    return connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id")
+}
+// pending find all the managers
+// function getEmployeesbyManagers (managerId) {
+//     return connection.query("SELECT employee.manager_id, employee.first_name, employee.last_name, WHERE employee.manager_id = ? group by employee.manager_id = ?",
+//     managerId)
+// }
+function getManagers () {
+    return connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id WHERE employee.manager_id != 'null'" )
 }
 
 function insertDepartment (department) {
@@ -310,14 +368,18 @@ function insertEmployee (employee) {
     return connection.query("INSERT INTO employee SET ?", employee);
 }
 
+function updateEmployeeRoleDB(employeeId, roleId) {
+    console.log("updaterole: ", employeeId, roleId)
+    return connection.query(
+      "UPDATE employee SET role_id = ? WHERE id = ?",
+      [roleId, employeeId]
+    );
+  }
 
-// function updateDepartment (department) {
-//     return connection.query("INSERT INTO department SET ?", department);
-// }
-// function updateRole (role) {
-//     return connection.query("INSERT INTO role SET ?", role);
-// }
-
-function updateEmployee (employee) {
-    return connection.query("INSERT INTO employee SET ?", employee);
-}
+  function updateEmployeeManagerDB(employeeId, managerId) {
+    console.log("updatemanager: ", employeeId, managerId)
+    return connection.query(
+      "UPDATE employee SET manager_id = ? WHERE id = ?",
+      [managerId, employeeId]
+    );
+  }
